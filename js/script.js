41c1f3be86f4a08a97d3c8fad62ab841559c9939
deepakkumarjha1323 <deepakkumarjha1323@gmail.com>
2023-06-21 18:53:59 +0530

    js arrays and rest operators

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Lets Create a Js Array
+
+var students = ['Raju', "Sohan", `Raja`, 'Rahul']; // [] is an Array
+var friends = ['Rahul', "Saurabh", `Nikhil`, 'Aman']; // [] is an Array
+
+// Console
+console.log(students[0]);
+console.log(students[1]);
+console.log(students[2]);
+console.log(students[3]);
+
+// Array can Store Multiple Data Type...
+let x = ['Deepak', 20, 5.6, true, ['Jha'], {address:'Masatipur'}];
+
+console.log(x);
+console.log(x[0]);
+console.log(x[1]);
+console.log(x[2]);
+console.log(x[3]);
+console.log(x[4]);
+console.log(x[5].address);
